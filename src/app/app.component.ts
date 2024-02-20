94a05123d7e9f491728d096765fe446906292ebf
raviojha96 <raviojha500@gmail.com>
2024-02-20 19:16:24 +0530

    header footer routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/layouts/header/header.component';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from './shared/layouts/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'a17-ecom';
